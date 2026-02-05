51dcf400bf272302317d856d408bedd7659e2315
tingsudo <tichen2010@gmail.com>
2026-02-04 20:14:51 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY", "sb_publishable_5MuzSf_1GiX94Zw8vzmITA_
 BASE_URL = os.getenv("BASE_URL", "https://reminder-tool-orcin.vercel.app/api/action")
 
 TEACHER_WEBHOOKS = {
-   # "Zhang": os.getenv("WEBHOOK_ZHANG", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6fb04ff9-89c2-4178-a873-224ec4ec6d11"),
-   # "Flora": os.getenv("WEBHOOK_FLORA", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=002a0ed4-74ff-47b0-ab72-576f6fb8e24e"),
-    "Ting": os.getenv("WEBHOOK_TING", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7aa9180a-4c39-4611-9303-7bf4d0ef8846")
-   # "Vivian": os.getenv("WEBHOOK_VIVIAN", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9211ef7c-3c53-428f-861d-7810e39be2f8"),
-   # "Mao": os.getenv("WEBHOOK_MAO", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5763b54b-a3eb-491e-900f-abb593395575")
+    "Zhang": os.getenv("WEBHOOK_ZHANG", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=6fb04ff9-89c2-4178-a873-224ec4ec6d11"),
+    "Flora": os.getenv("WEBHOOK_FLORA", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=002a0ed4-74ff-47b0-ab72-576f6fb8e24e"),
+    "Ting": os.getenv("WEBHOOK_TING", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7aa9180a-4c39-4611-9303-7bf4d0ef8846"),
+    "Vivian": os.getenv("WEBHOOK_VIVIAN", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9211ef7c-3c53-428f-861d-7810e39be2f8"),
+    "Mao": os.getenv("WEBHOOK_MAO", "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5763b54b-a3eb-491e-900f-abb593395575")
 }
 
 def get_pending_tasks():
